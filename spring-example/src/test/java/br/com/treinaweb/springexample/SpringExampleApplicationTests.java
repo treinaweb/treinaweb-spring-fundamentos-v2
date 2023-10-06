6097ec5db9bf538cc3f95afb1e4d7ca0afd063dc
Cleyson <cleysonph@gmail.com>
2023-10-06 03:34:30 -0300

    04:04 - Criando projeto Spring Boot com o Spring Initializr

---
+++ spring-example/src/test/java/br/com/treinaweb/springexample/SpringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.treinaweb.springexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
