1065100d92a1a213a3fd32b56d0dc3a967c9c748
Cleyson <cleysonph@gmail.com>
2023-10-10 16:24:34 -0300

    05:09 - Enviando dados do Controller para View com ModelAndView

--- spring-example/src/main/java/br/com/treinaweb/springexample/TestController.java
+++ spring-example/src/main/java/br/com/treinaweb/springexample/TestController.java
@@ -1,8 +1,11 @@
 package br.com.treinaweb.springexample;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class TestController {
@@ -11,6 +14,15 @@ public class TestController {
     public String action(ModelMap model) {
         model.addAttribute("nome", "TreinaWeb");
         return "teste";
+    }
+
+    @GetMapping("/teste2")
+    public ModelAndView action2() {
+        // var modelAndView = new ModelAndView("teste");
+        // modelAndView.addObject("nome", "TreinaWeb");
+        // return modelAndView;
+
+        return new ModelAndView("teste", Map.of("nome", "TreinaWeb"));
     }
     
 }
