3227f07650de4e6842bea9541363462df71a1298
Cleyson <cleysonph@gmail.com>
2023-10-10 17:08:23 -0300

    05:10 - Enviando dados da View para o Controller

--- spring-example/src/main/java/br/com/treinaweb/springexample/TestController.java
+++ spring-example/src/main/java/br/com/treinaweb/springexample/TestController.java
@@ -3,8 +3,11 @@ package br.com.treinaweb.springexample;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -23,6 +26,18 @@ public class TestController {
         // return modelAndView;
 
         return new ModelAndView("teste", Map.of("nome", "TreinaWeb"));
+    }
+
+    @GetMapping("/teste3")
+    public String action3(Model model) {
+        model.addAttribute("client", new Client("Cleyson", 27));
+        return "form";
+    }
+
+    @PostMapping("/teste3")
+    public String action4(Client client) {
+        System.out.println(client);
+        return "form";
     }
     
 }
