8fccba88c5e0950178de68c3726aa67c4a305085
Cleyson <cleysonph@gmail.com>
2023-10-11 14:23:57 -0300

    06:13 - Criando nosso primeiro model

--- spring-example/src/main/java/br/com/treinaweb/springexample/Client.java
+++ spring-example/src/main/java/br/com/treinaweb/springexample/Client.java
@@ -1,9 +1,22 @@
 package br.com.treinaweb.springexample;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     
+    @Column(length = 100, nullable = false)
     private String name;
 
+    @Column(nullable = false)
     private int age;
 
     public Client() {}
@@ -11,6 +24,20 @@ public class Client {
     public Client(String name, int age) {
         this.name = name;
         this.age = age;
+    }
+
+    public Client(Long id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getName() {
@@ -27,6 +54,31 @@ public class Client {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Client other = (Client) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
     @Override
