23932aba1ec390e0c033c0f604a6865b24baf344
Cleyson <cleysonph@gmail.com>
2023-10-11 15:00:16 -0300

    06:15 - Entendendo a injeção de depenicas do Spring

--- spring-example/src/main/java/br/com/treinaweb/springexample/SpringExampleApplication.java
+++ spring-example/src/main/java/br/com/treinaweb/springexample/SpringExampleApplication.java
@@ -1,13 +1,26 @@
 package br.com.treinaweb.springexample;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringExampleApplication {
+public class SpringExampleApplication implements CommandLineRunner {
+
+	private final ClientRepository clientRepository;
+
+	public SpringExampleApplication(ClientRepository clientRepository) {
+		this.clientRepository = clientRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(clientRepository);
+		System.out.println("A aplicação iniciou");
 	}
 
 }
