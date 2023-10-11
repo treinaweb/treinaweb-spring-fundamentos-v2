6fb18e18e7bf601e153809399a25f193d99c6d7f
Cleyson <cleysonph@gmail.com>
2023-10-11 15:48:01 -0300

    06:16 - Realizando operações de CRUD com o Spring Data JPA

--- spring-example/src/main/java/br/com/treinaweb/springexample/SpringExampleApplication.java
+++ spring-example/src/main/java/br/com/treinaweb/springexample/SpringExampleApplication.java
@@ -19,8 +19,28 @@ public class SpringExampleApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println(clientRepository);
-		System.out.println("A aplicação iniciou");
+		clientRepository.save(new Client("Cleyson", 27));
+
+		var clients = clientRepository.findAll();
+		System.out.println(clients);
+
+		var client1 = clientRepository.findById(1L);
+		var client2 = clientRepository.findById(2L);
+
+		if (client1.isPresent()) {
+			System.out.println("Client com ID 1 encontrado");
+			System.out.println(client1);
+		}
+
+		if (!client2.isPresent()) {
+			System.out.println("Client com ID 2 não encontrado");
+		}
+
+		client1.get().setName("Cleyson Editado");
+		clientRepository.save(client1.get());
+
+		// clientRepository.deleteById(1L);
+		clientRepository.delete(client1.get());
 	}
 
 }
