ee52947e0d4ccb51bddf9ff91606a354fff70f7c
Cleyson <cleysonph@gmail.com>
2023-10-11 13:29:35 -0300

    05:11 - Trabalhando com variáveis de rota

--- spring-example/src/main/java/br/com/treinaweb/springexample/TestController.java
+++ spring-example/src/main/java/br/com/treinaweb/springexample/TestController.java
@@ -1,5 +1,6 @@
 package br.com.treinaweb.springexample;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.stereotype.Controller;
@@ -7,7 +8,9 @@ import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -38,6 +41,30 @@ public class TestController {
     public String action4(Client client) {
         System.out.println(client);
         return "form";
+    }
+
+    // http://localhost:8080/teste5?age=27&skills=python,java,csharp
+    // http://localhost:8080/teste5?age=27&skills=python&skills=java&skills=csharp
+    @GetMapping("/teste5")
+    public String action5(
+        @RequestParam(name = "q", required = false, defaultValue = "treinaweb") 
+        String busca,
+        @RequestParam int age,
+        @RequestParam List<String> skills
+    ) {
+        System.out.println(busca);
+        System.out.println(age);
+        System.out.println(skills);
+        return "teste";
+    }
+
+    // http://localhost:8080/produtos/1
+    // http://localhost:8080/produtos/2
+    // http://localhost:8080/produtos/3
+    @GetMapping("/teste6/{id}/{name}")
+    public String action6(@PathVariable("id") Long productId) {
+        System.out.println(productId);
+        return "teste";
     }
     
 }
